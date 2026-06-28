57ba3ed7270230b0898266a2d19bc03af581e638
stefanova-kalina <stefanova_kalina@yahoo.com>
2026-06-28 09:54:05 +0100

    Initial Spring Boot Commit

---
+++ travel-forum/src/test/java/com/team_3/travel_forum/TravelForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_3.travel_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
