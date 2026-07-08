902aa02aea76bc04770c5b32391d5007f834ea10
stefanova-kalina <stefanova_kalina@yahoo.com>
2026-07-08 02:24:25 +0100

    Revert "adding Comment entity + repository and service"
    This reverts commit f8d48999a23a169734b9ae0b68427908caac3eef.

--- travel-forum/src/main/java/com/team_3/travel_forum/services/CommentService.java
+++ travel-forum/src/main/java/com/team_3/travel_forum/services/CommentService.java
@@ -1,25 +1,4 @@
 package com.team_3.travel_forum.services;
 
-import com.team_3.travel_forum.models.Comment;
-import com.team_3.travel_forum.models.User;
-
-import java.util.List;
-
-public interface CommentService {
-
-    List<Comment> get();
-
-    Comment get(int id);
-
-    void create(String content, int postId, User currentUser);
-
-    void update(int commentId, String updatedContent, User currentUser);
-
-    void delete(int commentId, User currentUser);
-
-    List<Comment> getCommentsByPost(int postId, User currentUser);
-
-    List<Comment> getCommentsByUser(int userId, User currentUser);
-
-    long countByPost(int postId);
+public class CommentService {
 }
