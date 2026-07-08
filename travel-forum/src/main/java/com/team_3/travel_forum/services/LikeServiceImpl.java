5c0e2a45e7d3241cb1313e915a4c6e05ac42b25c
stefanova-kalina <stefanova_kalina@yahoo.com>
2026-07-08 16:37:20 +0100

    adding Like entity + repository and service + custom EntityNotFoundException

---
+++ travel-forum/src/main/java/com/team_3/travel_forum/services/LikeServiceImpl.java
@@ -0,0 +1,77 @@
+package com.team_3.travel_forum.services;
+
+import com.team_3.travel_forum.exceptions.BlockedUserException;
+import com.team_3.travel_forum.exceptions.EntityNotFoundException;
+import com.team_3.travel_forum.exceptions.UnauthorizedAccessException;
+import com.team_3.travel_forum.models.Like;
+import com.team_3.travel_forum.models.Post;
+import com.team_3.travel_forum.models.User;
+import com.team_3.travel_forum.repositories.LikeRepository;
+import com.team_3.travel_forum.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikeServiceImpl implements LikeService {
+
+    private final LikeRepository likeRepository;
+    private final PostRepository postRepository;
+
+    @Autowired
+    public LikeServiceImpl(LikeRepository likeRepository, PostRepository postRepository) {
+        this.likeRepository = likeRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public void addLike(int postId, User currentUser) {
+        if (currentUser == null) {
+            throw new UnauthorizedAccessException("You must be logged in to like a post.");
+        }
+
+        //TODO discuss if blocked users should be able to like posts
+        if (currentUser.isBlocked()) {
+            throw new BlockedUserException("Your account has been blocked and you cannot like posts.");
+        }
+
+        Post currentPost = postRepository.get(postId);
+
+        if (likeRepository.findByPostAndUser(postId, currentUser.getId()) != null) {
+            return; //post already liked, nothing to do
+        }
+
+        Like newLike = new Like();
+        newLike.setUser(currentUser);
+
+        newLike.setPost(currentPost);
+
+        likeRepository.create(newLike);
+
+    }
+
+    @Override
+    public void removeLike(int postId, User currentUser) {
+        if (currentUser == null) {
+            throw new UnauthorizedAccessException("You must be logged in to unlike a post.");
+        }
+        //TODO discuss if blocked users should be able to like posts
+        if (currentUser.isBlocked()) {
+            throw new BlockedUserException("Your account has been blocked and you cannot unlike posts.");
+        }
+
+        postRepository.get(postId);
+
+        Like existingLike = likeRepository.findByPostAndUser(postId, currentUser.getId());
+        if (existingLike == null) {
+            throw new EntityNotFoundException("You have not liked this post yet.");
+        }
+
+        likeRepository.delete(existingLike.getId());
+    }
+
+    @Override
+    public long getLikeCountForPost(int postId) {
+        postRepository.get(postId);
+        return likeRepository.countByPost(postId);
+    }
+}
