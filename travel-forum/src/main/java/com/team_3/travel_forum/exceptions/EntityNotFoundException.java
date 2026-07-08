ec4fe96d74ec523b5de1cf967aa6d554b333df21
stefanova-kalina <stefanova_kalina@yahoo.com>
2026-07-08 02:24:37 +0100

    Revert "adding custom exceptions - EntityNotFoundException and UnauthorisedAccessException"
    This reverts commit 5cd7a873fd9529a16792d71f3de2a46f169711f8.

--- travel-forum/src/main/java/com/team_3/travel_forum/exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.team_3.travel_forum.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-
-}
--- travel-forum/src/main/java/com/team_3/travel_forum/exceptions/UnauthorisedAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.team_3.travel_forum.exceptions;
-
-public class UnauthorisedAccessException extends RuntimeException {
-    public UnauthorisedAccessException(String message) {
-        super(message);
-    }
-}
